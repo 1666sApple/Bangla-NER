3679fda49711b87c1206b429cfa996cdd04a5331
1666sApple <ananno.034@gmail.com>
2024-08-28 09:48:45 +0600

    Modify dataset.py

--- model/dataset.py
+++ model/dataset.py
@@ -7,41 +7,60 @@ class CustomDataset(Dataset):
         self.texts = texts
         self.pos_tags = pos_tags
         self.ner_tags = ner_tags
-  
+
     def __len__(self):
         return len(self.texts)
 
     def __getitem__(self, index):
-        texts = self.texts[index]
+        text = self.texts[index]
         pos_tags = self.pos_tags[index]
         ner_tags = self.ner_tags[index]
-    
-        ids, target_pos, target_ner = [], [], []
 
-        for i, s in enumerate(texts):
-            inputs = Config.TOKENIZER.encode(s, add_special_tokens=False)
-            input_len = len(inputs)
-            ids.extend(inputs)
-            target_pos.extend(input_len * [pos_tags[i]])
-            target_ner.extend(input_len * [ner_tags[i]])
+        if isinstance(text, list):
+            text = ' '.join(text)
+
+        encoding = Config.TOKENIZER.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=Config.MAX_LEN,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt',
+            return_attention_mask=True,
+            return_token_type_ids=True,
+            return_offsets_mapping=True
+        )
+
+        input_ids = encoding['input_ids'].squeeze().tolist()
+        attention_mask = encoding['attention_mask'].squeeze().tolist()
+        token_type_ids = encoding['token_type_ids'].squeeze().tolist()
+        offsets = encoding['offset_mapping'].squeeze().tolist()
+
+        token_to_tag_pos = [Config.VALUE_TOKEN] * len(input_ids)
+        token_to_tag_ner = [Config.VALUE_TOKEN] * len(input_ids)
         
-        ids = ids[:Config.MAX_LEN - 2]
-        target_pos = target_pos[:Config.MAX_LEN - 2]
-        target_ner = target_ner[:Config.MAX_LEN - 2]
-
-        ids = Config.CLS + ids + Config.SEP
-        target_pos = Config.VALUE_TOKEN + target_pos + Config.VALUE_TOKEN
-        target_ner = Config.VALUE_TOKEN + target_ner + Config.VALUE_TOKEN
-
+        tag_map_pos = [Config.VALUE_TOKEN] * len(input_ids)
+        tag_map_ner = [Config.VALUE_TOKEN] * len(input_ids)
+        
+        for i, (start, end) in enumerate(offsets):
+            if start != 0 and end != 0:
+                token_index = sum(1 for s, e in offsets[:i] if s != 0 and e != 0) - 1
+                if token_index < len(pos_tags):
+                    tag_map_pos[i] = pos_tags[token_index]
+                    tag_map_ner[i] = ner_tags[token_index]
+        
+        ids = [Config.CLS] + input_ids[1:-1] + [Config.SEP]
+        target_pos = [Config.VALUE_TOKEN] + tag_map_pos[1:-1] + [Config.VALUE_TOKEN]
+        target_ner = [Config.VALUE_TOKEN] + tag_map_ner[1:-1] + [Config.VALUE_TOKEN]
         mask = [1] * len(ids)
         token_type_ids = [0] * len(ids)
 
         padding_len = Config.MAX_LEN - len(ids)
-        ids = ids + ([1] * padding_len)
-        target_pos = target_pos + ([1] * padding_len)
-        target_ner = target_ner + ([1] * padding_len)
-        mask = mask + ([0] * padding_len)
-        token_type_ids = token_type_ids + ([0] * padding_len)
+        ids.extend([Config.VALUE_TOKEN] * padding_len)
+        target_pos.extend([Config.VALUE_TOKEN] * padding_len)
+        target_ner.extend([Config.VALUE_TOKEN] * padding_len)
+        mask.extend([0] * padding_len)
+        token_type_ids.extend([0] * padding_len)
 
         return {
             "ids": torch.tensor(ids, dtype=torch.long),
